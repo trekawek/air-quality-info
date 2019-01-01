4b4b0ab2b99735e61a81d159b4c83fea087e3d58
Tomek Rękawek <rekawek@adobe.com>
2019-01-01 20:19:55 +0100

    Redirect to the first device if none is given.

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,14 +2,37 @@
 require_once('config.php');
 require_once('lib/pollution_levels.php');
 require_once('lib/rrd.php');
-require_once('lib/utils.php');
+
+function l($device, $action, $query_args = array()) {
+  $link = '/'.$device['name'];
+
+  if ($action != 'sensors') {
+    $link .= '/'.$action;
+  }
+  
+  $query_arg_added = false;
+  foreach ($query_args as $k => $v) {
+    if ($query_arg_added) {
+      $link .= '&';
+    } else {
+      $link .= '?';
+      $query_arg_added = true;
+    }
+    $link .= "${k}=${v}";
+  }
+
+  if ($link == '') {
+    $link = '/';
+  }
+
+  return $link;
+}
 
 list($uri) = explode('?', $_SERVER['REQUEST_URI']);
 $uri = explode('/', $uri);
 $uri = array_values(array_filter($uri));
 
-$device = CONFIG['devices'][0];
-
+$device = null;
 if (count($uri) > 0) {
   foreach (CONFIG['devices'] as $d) {
     if ($uri[0] == $d['name']) {
@@ -24,6 +47,13 @@ if (count($uri) > 0) {
   $current_action = array_shift($uri);
 } else {
   $current_action = 'sensors';
+}
+
+if ($device == null) {
+  header('Location: '
+    .l(CONFIG['devices'][0], $current_action)
+    .(isset($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : ''));
+  exit;
 }
 
 switch ($current_action) {
