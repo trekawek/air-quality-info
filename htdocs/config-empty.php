1f3b0985b32133feb8df8e2166bb2b26ee30e9f5
Tomek Rękawek <rekawek@adobe.com>
2019-01-03 21:57:03 +0100

    Configurable JSON field names.

--- htdocs/config-empty.php
+++ htdocs/config-empty.php
@@ -7,6 +7,13 @@ define('CONFIG', array(
         'esp8266id'   => '...',
         'name'        => 'main',         # this will be used in URLs
         'description' => 'Main location', # user-friendly location name, will be used in navbar
+        'value_mapping' => array(
+            'pm10'        => 'SDS_P1',
+            'pm25'        => 'SDS_P2',
+            # 'temperature' => 'BME280_temperature',
+            # 'humidity'    => 'BME280_humidity',
+            # 'pressure'    => 'BME280_pressure',
+          ),
         # 'maintenance' => 'Trwa przerwa techniczna. Detektor zostanie uruchomiony ponownie w ciągu godziny.'
     ),
 #    array(
--- htdocs/update.php
+++ htdocs/update.php
@@ -1,4 +1,15 @@
 <?php
+function read_value($device, $value_name, $sensor_values, $undefined_value = null) {
+  $value = null;
+  if (isset($device['value_mapping'][$value_name])) {
+    $mapped_name = $device['value_mapping'][$value_name];
+    if ($mapped_name && isset($sensor_values[$mapped_name])) {
+      $value = $sensor_values[$mapped_name];
+    }
+  }
+  return $value == null ? $undefined_value : $value;
+}
+
 if (!($_SERVER['PHP_AUTH_USER'] == $device['user'] && $_SERVER['PHP_AUTH_PW'] == $device['password'])) {
   header('WWW-Authenticate: Basic realm="data.php"');
   header('HTTP/1.0 401 Unauthorized');
@@ -14,7 +25,22 @@ foreach ($sensors as $row) {
   $map[$row['value_type']] = $row['value'];
 }
 
-update_rrd($device['esp8266id'], time(), $map['SDS_P2'], $map['SDS_P1'], $map['BME280_temperature'], $map['BME280_pressure'] / 100, $map['BME280_humidity']);
+if ($device['esp8266id'] != $data['esp8266id']) {
+  error_log('esp8266id mismatch. Expected: '.$device['esp8266id'].' but got '.$data['esp8266id']);
+  exit;
+}
+
+$time = time();
+
+update_rrd(
+  $device['esp8266id'],
+  $time,
+  read_value($device, 'pm25', $map, 'U'),
+  read_value($device, 'pm10', $map, 'U'),
+  read_value($device, 'temperature', $map, 'U'),
+  read_value($device, 'pressure', $map, 'U') / 100,
+  read_value($device, 'humidity', $map, 'U')
+);
 
 if (CONFIG['store_json_payload']) {
   file_put_contents('data/'.$device['esp8266id'].'.json', $payload);
