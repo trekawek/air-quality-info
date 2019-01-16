16248d72956d4669483bb389115a44c4e08d1bd7
Tomek Rękawek <rekawek@adobe.com>
2019-01-16 14:38:03 +0100

    Allow to configure MySQL.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -7,8 +7,6 @@ require_once('lib/math.php');
 require_once('lib/pollution_levels.php');
 require_once('lib/themes.php');
 require_once('db/dao.php');
-require_once('db/rrd.php');
-require_once('db/mysql.php');
 
 function l($device, $action, $query_args = array()) {
   $link = '/'.$device['name'];
@@ -63,7 +61,19 @@ if ($device == null) {
   exit;
 }
 
-$dao = new RRRDao($device['esp8266id']);
+switch (CONFIG['db']['type']) {
+  case 'mysql':
+  require_once('db/mysql.php');
+  $mysqli = new mysqli(CONFIG['db']['host'], CONFIG['db']['user'], CONFIG['db']['password'], CONFIG['db']['name']);
+  $dao = new MysqlDao($device['esp8266id'], $mysqli);
+  break;
+
+  default:
+  case 'rrd':
+  require_once('db/rrd.php');
+  $dao = new RRRDao($device['esp8266id']);
+  break;
+}
 
 switch ($current_action) {
   case 'update':
