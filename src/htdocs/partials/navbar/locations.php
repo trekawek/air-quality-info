72282c238026d063f5edd3ec01dbb2decf525d14
Tomek Rękawek <rekawek@adobe.com>
2019-05-28 13:04:56 +0200

    Display locations in case there's a single root directory.

--- src/htdocs/partials/navbar/locations.php
+++ src/htdocs/partials/navbar/locations.php
@@ -1,5 +1,5 @@
 <?php include('directory.php'); ?>
-<?php if (count($deviceTree['children']) > 1): ?>
+<?php if (count($deviceTree['children']) > 1 || $deviceTree['device_id'] === null): ?>
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     <?php echo __('Locations') ?>
