b65d90aa050e4b275f9d8a193dcbfe1a9770569d
Tomek Rękawek <rekawek@adobe.com>
2019-05-28 17:22:56 +0200

    Removed name from the footer.

--- src/htdocs/admin/partials/about/tail.php
+++ src/htdocs/admin/partials/about/tail.php
@@ -1,7 +1,7 @@
     </div>
 
 <footer class="pt-4 my-md-5 pt-md-5 border-top text-center">
-    &copy; 2019 air.eco - Tomasz Rękawek
+    &copy; 2019 <span class="logo">aqi.eco</span>
 </footer>
 </body>
 </html>
