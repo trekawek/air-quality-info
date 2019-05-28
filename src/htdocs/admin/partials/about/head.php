a3b5c3c1483141ab6e9f3ff88d5696e433c4cfc5
Tomek Rękawek <rekawek@adobe.com>
2019-05-28 18:43:54 +0200

    Enable toggler for the menu in about page.

--- src/htdocs/admin/partials/about/head.php
+++ src/htdocs/admin/partials/about/head.php
@@ -21,6 +21,7 @@ gtag('config', '<?php echo CONFIG['ga_id']; ?>');
         <link rel="stylesheet" href="/public/css/vendor.min.css"/>
         <link rel="stylesheet" href="/public/css/themes/default.min.css"/>
         <link rel="stylesheet" href="/admin/public/css/landing.css"/>
+        
     </head>
 
 <body>
@@ -30,6 +31,9 @@ gtag('config', '<?php echo CONFIG['ga_id']; ?>');
         <a class="navbar-brand" href="/">
             <img src="/public/img/aqi.png"/><span class="logo">aqi.eco</span>
         </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <?php navItem(array('main', 'index'), 'Home'); ?>
--- src/htdocs/admin/partials/about/tail.php
+++ src/htdocs/admin/partials/about/tail.php
@@ -3,5 +3,6 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top text-center">
     &copy; 2019 <span class="logo">aqi.eco</span>
 </footer>
+<script src="/admin/public/js/vendor.min.js"></script>
 </body>
 </html>
