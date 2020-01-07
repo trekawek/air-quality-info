51bcbefc45014da7f4e1b848767a984f0e35388a
Tomek Rękawek <rekawek@adobe.com>
2020-01-07 21:15:13 +0100

    Updated the default configuration with the minio setup.

--- config-DEFAULT.php
+++ config-DEFAULT.php
@@ -7,6 +7,17 @@ define('CONFIG', array(
     'password' => 'test',
     'name' => 'air_quality_info'
   ),
+  's3' => array(
+    'version' => 'latest',
+    'region'  => 'us-east-1',
+    'endpoint' => 'http://minio:9000',
+    'use_path_style_endpoint' => true,
+    'credentials' => array(
+      'key'    => 'minio',
+      'secret' => 'minio123',
+    )
+  ),
+  's3Bucket' => 'test-bucket',
   'enable_pwa' => false,
   'admin_domains' => array('aqi.eco.localhost'),
   'user_domain_suffixes' => array('.aqi.eco.localhost'),
