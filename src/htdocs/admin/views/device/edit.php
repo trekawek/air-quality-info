d094777d5b080bc399e5ad167d380f7eac63afc0
Tomek Rękawek <rekawek@adobe.com>
2020-02-06 12:58:32 +0100

    Remove input fields for the device settings.

--- src/htdocs/admin/views/device/edit.php
+++ src/htdocs/admin/views/device/edit.php
@@ -25,18 +25,18 @@
             </div>
             <div class="card-body">
                 <p><?php echo __('Please use following data to configure your sensor') ?>:</p>
-                <dl class="row">
-                    <dt class="col-lg-6"><?php echo __('Send data to own API') ?></dt>
-                    <dd class="col-lg-6"><input type="checkbox" class="form-control" checked disabled/></dd>
+                <dl>
+                    <dt><?php echo __('Send data to own API') ?></dt>
+                    <dd><input type="checkbox" class="form-control" checked disabled/></dd>
 
-                    <dt class="col-lg-6"><?php echo __('Server') ?></dt>
-                    <dd class="col-lg-6"><input type="text" class="form-control" value="<?php echo 'api' . CONFIG['user_domain_suffixes'][0] ?>" disabled/></dd>
+                    <dt><?php echo __('Server') ?></dt>
+                    <dd class="text-monospace"><?php echo 'api' . CONFIG['user_domain_suffixes'][0] ?></dd>
 
-                    <dt class="col-lg-6"><?php echo __('Path') ?></dt>
-                    <dd class="col-lg-6"><input type="text" class="form-control" value="/update/<?php echo $device['api_key']; ?>" disabled/></dd>
+                    <dt><?php echo __('Path') ?></dt>
+                    <dd class="text-monospace">/update/<?php echo $device['api_key']; ?></dd>
 
-                    <dt class="col-lg-6"><?php echo __('Port') ?></dt>
-                    <dd class="col-lg-6"><input type="text" class="form-control" value="443" disabled/></dd>
+                    <dt><?php echo __('Port') ?></dt>
+                    <dd class="text-monospace">443</dd>
                 </dl>
             </div>
         </div>
