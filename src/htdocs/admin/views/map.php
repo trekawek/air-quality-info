a90e2eb4f5c35958bc4ca41374615d523a2062e2
Tomek Rękawek <rekawek@adobe.com>
2020-02-22 21:10:16 +0100

    Fixed map.

--- src/htdocs/admin/views/map.php
+++ src/htdocs/admin/views/map.php
@@ -5,6 +5,6 @@
     </div>
 </div>
 <script src="/public/js/vendor.min.js?v=31"></script>
-<script src="/public/js/main.js?v=39"></script>
-<script src="/public/js/map.js?v=39"></script>
+<script src="/public/js/main.js?v=40"></script>
+<script src="/public/js/map.js?v=40"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo CONFIG['google_maps_key'] ?>&callback=mapsLoaded" async defer></script>
