ac88cedec39d1e430ac8ac52ba4a39fa3c9555b3
Tomek Rękawek <rekawek@adobe.com>
2020-02-27 08:45:50 +0100

    Added widgets to the Romanian translation.

--- src/htdocs/locale/ro.php
+++ src/htdocs/locale/ro.php
@@ -212,4 +212,157 @@ $locale['Widgets'] = 'Widgets';
 $locale['Widgets created here will show the average air quality calculated from all sensors.'] = 'Widget-urile create aici vor arăta calitatea medie a aerului calculată de la toți senzorii.';
 $locale['Widgets created here will show the state of a single device.'] = 'Widget-urile create aici vor arăta starea unui singur dispozitiv.';
 $locale['Year'] = 'An';
+
+$locale['_widgets'] = [
+    0 => [
+        'label' => 'Okay',
+        'icon' => 'fa-smile-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'walks outside'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'physical activity outside'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'spending time outdoors'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'spending time outdoors (pregnant, asthmatic, elderly)'
+            ],
+        ]
+    ],
+    1 => [
+        'label' => 'Moderate',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'walks outside'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'physical activity outside'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'spending time outdoors'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Not recommended',
+                'description' => 'spending time outdoors (pregnant, asthmatic, elderly)'
+            ],
+        ]
+    ],
+    2 => [
+        'label' => 'Mediocre',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Acceptable',
+                'description' => 'walks outside'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Acceptable',
+                'description' => 'physical activity outside'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'orange',
+                'label' => 'Not recommended',
+                'description' => 'prolonged exposure in the open air'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Not recommended',
+                'description' => 'prolonged exposure in the open air (pregnant, asthmatic, elderly)'
+            ],
+        ]
+    ],
+    3 => [
+        'label' => 'Unfavorable',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'Staying at home (elderly, pregnant women, people with heart and respiratory diseases)'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'orange',
+                'label' => 'Not recommended',
+                'description' => 'walks outside'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'orange',
+                'label' => 'Not recommended',
+                'description' => 'physical activity outside'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'orange',
+                'label' => 'Limit',
+                'description' => 'driving a car'
+            ],
+        ]
+    ],
+    4 => [
+        'label' => 'Bad',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Recommended',
+                'description' => 'staying at home'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'red',
+                'label' => 'Postpone',
+                'description' => 'walks outside'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'red',
+                'label' => 'Postpone',
+                'description' => 'physical activity outside'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'red',
+                'label' => 'Postpone',
+                'description' => 'driving a car'
+            ],
+        ]
+    ]
+];
 ?>
