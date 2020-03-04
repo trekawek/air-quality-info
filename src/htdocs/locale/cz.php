2f64b1939d40ba068b488e65b80111b98d65b1cf
rhoracek <61061873+rhoracek@users.noreply.github.com>
2020-03-04 12:00:02 +0100

    cz.php
    complete translation into Czech language

---
+++ src/htdocs/locale/cz.php
@@ -0,0 +1,386 @@
+<?php
+/*$jsLocale = array();
+$jsLocale['days'] = 'Dny';
+$jsLocale['Are you sure to delete this resource?'] = 'Chcete vymazat tento zdroj?';
+$jsLocale['Days'] = 'Dny';
+$jsLocale['Detector humidity'] = 'Čidlo vlhkosti';
+$jsLocale['Detector temperature'] = 'Čidlo teploty';
+$jsLocale['High'] = 'Vysoká';
+$jsLocale['Humidity'] = 'Vlhkost';
+$jsLocale['Low'] = 'Nízká';
+$jsLocale['Medium'] = 'Střední';
+$jsLocale['Night view'] = 'Noční zobrazení';
+$jsLocale['PM₁₀ limit'] = 'PM₁₀ limit';
+$jsLocale['PM₂₅ limit'] = 'PM₂₅ limit';
+$jsLocale['Pollution level'] = 'Úroveň znečištění';
+$jsLocale['Pressure'] = 'Tlak';
+$jsLocale['Temperature'] = 'Teplota';
+$jsLocale['Very high'] = 'Velmi vysoká';
+$jsLocale['Very low'] = 'Velmi nízká';
+
+$locale = array_merge($jsLocale);
+$locale['<meta> tags to be put in the <head> section'] = '<meta> značky, které mají být vloženy do sekce <head>';
+$locale['%s is out of range'] = '% jsou mimo rozsah';
+$locale['<a href="https://www.airqualitynow.eu/about_indices_definition.php">CAQI</a> index'] = '<a href="https://www.airqualitynow.eu/about_indices_definition.php">CAQI</a> index';
+$locale['About page body'] = 'O webu - obsah stránky';
+$locale['About page name (3)'] = 'O webu - název stránky';
+$locale['Account'] = 'Účet';
+$locale['Account info'] = 'Informace o účtu';
+$locale['Add custom field mapping'] = 'Přidat vlastní mapping';
+$locale['Add mapping'] = 'Přidat mapping';
+$locale['Add new device'] = 'Přidat nové zařízení';
+$locale['Add new directory'] = 'Přidat nový adresář';
+$locale['Add new widget'] = 'Přidat nový widget';
+$locale['Add sensor id'] = 'Přidat senzor id';
+$locale['Add your own device'] = 'Přidat vlastní zařízení';
+$locale['Added new sensor id'] = 'Přidáno nový senzor id';
+$locale['Air quality'] = 'Kvalita ovzduší';
+$locale['Allows sensor.community sensors'] = 'Umožňuje sensor.community senzory';
+$locale['An account with this e-mail address is already registered.'] = 'Účet s tímto uživatelským jménem již existuje';
+$locale['Annual average'] = 'Roční průměr';
+$locale['Annual averages'] = 'Roční průměry';
+$locale['Annual stats'] = 'roční statistiky';
+$locale['Atmospheric pressure'] = 'Atmosférický tlak';
+$locale['Average'] = 'Průměr';
+$locale['Back to the device'] = 'Zpět na zařízení';
+$locale['Brand name (2)'] = 'Jméno (2)';
+$locale['Choose location'] = 'Zvolte umístění';
+$locale['Click to see more details.'] = 'Klikněte pro více podrobností';
+$locale['Contact info'] = 'Kontakt';
+$locale['Create'] = 'Vytvořit';
+$locale['Create directory'] = 'Vytvořit adresář';
+$locale['Create your account'] = 'Vytvořit účet';
+$locale['Create widget'] = 'Vytvořit widget';
+$locale['Created a new device'] = 'Vytvořit nové zařízení';
+$locale['Created a new directory'] = 'Vytvořit nový adresář';
+$locale['Created a new mapping'] = 'Vytvořit nový mapping';
+$locale['Created a new widget'] = 'Vytvořit nový widget';
+$locale['CSV'] = 'CSV';
+$locale['CSV archive'] = 'CSV archiv';
+$locale['Custom CSS style'] = 'Uživatelský CSV styl';
+$locale['Current air quality'] = 'Aktuální kvalita vzduchu';
+$locale['Current brand icon'] = 'Aktuální ikona';
+$locale['Custom field mappings'] = 'Uživatelské mapovaní JSON';
+$locale['Daily graph'] = 'Denní graf';
+$locale['Dashboard'] = 'Nástěnka';
+$locale['Darkly theme'] = 'Tmavé téma';
+$locale['Data'] = 'Data';
+$locale['Database field'] = 'Databázové pole';
+$locale['Day'] = 'Den';
+$locale['Days with PM₁₀ above 50µg/m³'] = 'Dny s překročením PM₁₀ přes 50µg/m³';
+$locale['Debug info'] = 'Diagnostické informace';
+$locale['Deleted the device'] = 'Vymazat zařízení';
+$locale['Deleted the mapping'] = 'Vymazat mapping';
+$locale['Deleted the node'] = 'Vymazat';
+$locale['Deleted the sensor id'] = 'Vymazat senzor id';
+$locale['Description'] = 'Popis';
+$locale['Default theme'] = 'Základní nastavení';
+$locale['Deleted widget'] = 'Vymazat widget';
+$locale['Device hierarchy'] = 'Struktura zařízení';
+$locale['Device JSON list'] = 'Seznam JSON zařízení';
+$locale['Device list'] = 'Seznam zařízení';
+$locale['Device name'] = 'Jméno zařízení';
+$locale['Device'] = 'Zařízení';
+$locale['Device widget list'] = 'Seznam widgetů zařízení';
+$locale['Devices'] = 'Zařízení';
+$locale['Domain'] = 'Doména';
+$locale['Domain name'] = 'Jméno domény';
+$locale['Domain widget list'] = 'Seznam widgetů domény';
+$locale['E-mail'] = 'E-mail';
+$locale['Edit sensor'] = 'Upravit senzor';
+$locale['Edit widget'] = 'Upravit widget';
+$locale['Enabling the sensor.community support disables the templating options.'] = 'Zapnutí podpory sensor.community vypne možnost šablonování';
+$locale['ESP 8266 id'] = 'ESP 8266 id';
+$locale['ESP8266 ID'] = 'ESP8266 ID';
+$locale['Edit device'] = 'Upravit zařízení';
+$locale['Edit directory'] = 'Upravit adresář';
+$locale['Elevation (m a.s.l.)'] = 'Nadmořská výška (m.n.m.)';
+$locale['Extra description'] = 'Extra popis';
+$locale['Fill to add a new item to the menu.'] = 'Vyplňte pro přidání nové nabídky do menu';
+$locale['Find sensor on'] = 'Hledání senzorů zapnuto';
+$locale['Footer'] = 'Zápatí';
+$locale['Footer (5)'] = 'Zápatí (5)';
+$locale['Graphs'] = 'Grafy';
+$locale['Guide'] = 'Průvodce';
+$locale['Header (4)'] = 'Záhlaví (4)';
+$locale['Hidden'] = 'Skrytý';
+$locale['Home'] = 'Domů';
+$locale['Horizontal'] = 'Horizontální';
+$locale['Import data from Madavi.de'] = 'Importovat data z Madavi.de';
+$locale['Imports data from'] = 'Importovat data z';
+$locale['Import finished'] = 'Import dokončen';
+$locale['Index'] = 'Index';
+$locale['Instantaneous'] = 'Okamžité';
+$locale['Invalid domain'] = 'Chybná doména';
+$locale['Invalid email or password'] = 'Chybný email nebo heslo';
+$locale['Invalid path'] = 'Chybná cesta';
+$locale['JSON field'] = 'JSON field';
+$locale['JSONs'] = 'JSONs';
+$locale['Last data'] = 'Poslední data';
+$locale['Last modification'] = 'Poslední úpravy';
+$locale['Last received data'] = 'Poslední přijatá data';
+$locale['Last update'] = 'Poslední update';
+$locale['Latitude'] = 'Zeměpisná šířka';
+$locale['Link'] = 'Odkaz';
+$locale['Link device'] = 'Odkaz na zařízení';
+$locale['Link external device'] = 'Odkaz na externí zařízení';
+$locale['Link sensor.community device'] = 'Odkaz na sensor.community zařízení';
+$locale['Linked device'] = 'Přiřazené zařízení';
+$locale['Loading...'] = 'Načítání...';
+$locale['Locations'] = 'Umístění';
+$locale['login'] = 'přihlášení';
+$locale['Login'] = 'Přihlášení';
+$locale['Logout'] = 'Odhlášení';
+$locale['Longitude'] = 'Zeměpisná délka';
+$locale['Maintenance tools'] = 'Nástroj pro údržbu';
+$locale['Make device default'] = 'zvoli jako defaultní zařízení';
+$locale['Map'] = 'Map';
+$locale['Maximum allowed size is 256 kB.'] = 'Maximální povolená velikost je 256 kB';
+$locale['Month'] = 'Měsíc';
+$locale['Moving average'] = 'Průměr';
+$locale['Name'] = 'Jméno';
+$locale['New password has been set.'] = 'Nové heslo bylo nastaveno';
+$locale['Only PNGs and SVGs are allowed'] = 'Povoleny jsou pouze PNG a SVG';
+$locale['Open'] = 'Otevřeno';
+$locale['Operations'] = 'Činnosti';
+$locale['Page unavailable offline'] = 'Stránka nedostupná offline';
+$locale['Password'] = 'Heslo';
+$locale['Path'] = 'Cesta';
+$locale["Please choose value between 50 and 500."] = 'Zvolte prosím hodnotu v rozpětí 50 až 500';
+$locale["Please create an account if you don't have one already."] = 'Vytvořte si účet, pokud jej ještě nemáte';
+$locale['Please connect to the internet to get the most recent info.'] = 'Chcete-li získat nejnovější informace, připojte se k internetu';
+$locale['Please paste the URL address of the device you want to link. It must be a link within aqi.eco domain, eg.: https://smolna.aqi.eco/13b'] = 'Vložte prosím adresu URL zařízení, které chcete propojit. Musí to být odkaz v doméně aqi.eco, např .: https://smolna.aqi.eco/13b';
+$locale['Please provide a valid e-mail address.'] = 'Uveďte prosím platnou e-mailovou adresu';
+$locale['Please provide two identical passwords.'] = 'Uveďte prosím dvakrát stejné heslo';
+$locale['Please use following data to configure your sensor'] = 'Ke konfiguraci vašeho senzoru použijte prosím následující data';
+$locale['PM'] = 'PM';
+$locale['Pollution levels by days'] = 'Úroveň znečištění podle dnů';
+$locale['Port'] = 'Port';
+$locale['Powered by '] = 'Vytvořeno';
+$locale['Radius (m)'] = 'Polomě (m)';
+$locale['Range'] = 'Rozsah';
+$locale['Redirect home page'] = 'Přesměrovat home page';
+$locale['Received JSONs'] = 'Přijaté JSONs';
+$locale['register'] = 'registrovat';
+$locale['Register'] = 'Registrovat';
+$locale['Register now!'] = 'Registrovat nyní!';
+$locale['Remove brand icon'] = 'Odstranit ikonu';
+$locale['Repeat password'] = 'Opakovat heslo';
+$locale['Reset HTTP password'] = 'Změnit HTTP heslo ';
+$locale['Root'] = 'Root';
+$locale['Send data to own API'] = 'Poslat data na Vaše API';
+$locale['Sensor configuration'] = 'Konfigurace senzoru';
+$locale['Sensor ID'] = 'Senzor ID';
+$locale['Sensor id'] = 'senzor id';
+$locale['Sensord id list'] = 'Seznam id senzorů';
+$locale['Server'] = 'Server';
+$locale['Settings'] = 'Nastavení';
+$locale['Show all'] = 'Zobrazit vše';
+$locale['Show widget on the group page'] = 'Zobrazit widget na stránce skupin';
+$locale['Show widget on the sensor page'] = 'Zobrazit widget na stránce senzorů';
+$locale['Sign in'] = 'Přihlášení';
+$locale['Sign in to your account'] = 'Přihlásit k účtu';
+$locale['Sign up'] = 'Zaregistrování';
+$locale['Size'] = 'Velikost';
+$locale['Start'] = 'Start';
+$locale['Stats'] = 'Statistiky';
+$locale['Support'] = 'Podpora';
+$locale['Template widgets'] = 'Šablony widgetů';
+$locale['Templates'] = 'Šablony';
+$locale['The domain name has to consists of letters and digits.'] = 'Název domény musí obsahovat písmena a číslice';
+$locale['The minimum length of the password is 8 characters.'] = 'Minimální délka hesla je 8 znaků.';
+$locale['The name should consist of alphanumeric characters and dashes'] = 'Název by měl obsahovat alfanumerické znaky a pomlčky';
+$locale['The path should consist of alphanumeric characters, dashes and slashes'] = 'Cesta by měla obsahovat alfanumerické znaky, pomlčky a lomítka';
+$locale['Theme'] = 'Téma';
+$locale['There are no data'] = 'Žádná data';
+$locale['This domain is already used.'] = 'Tato doména je již užita';
+$locale['This is default device'] = 'Toto je základní zařízení';
+$locale['This is the HTML content of the custom page linked above.'] = 'Toto je obsah HTML vlastní stránky propojené výše';
+$locale['Time'] = 'Čas';
+$locale['Timezone'] = 'Časová zóna';
+$locale['Title'] = 'Název';
+$locale['Update'] = 'Update';
+$locale['Update brand icon (1)'] = 'Změnit icon (1)';
+$locale['Updated directory'] = 'Změnit adresář';
+$locale['Updated settings'] = 'Změnit nastavení';
+$locale['Updated template'] = 'Změnit šablonu';
+$locale['Updated the default device'] = 'Změnit základní zařízení';
+$locale['Updated the device'] = 'Změnit zařízení';
+$locale['Updated the password'] = 'Změnit heslo';
+$locale['Updated widget'] = 'Změnit widget';
+$locale['Use PNG picture'] = 'Použít PNG obrázek';
+$locale['Use as a favicon'] = 'Použít jako favicon';
+$locale['User'] = 'Uživatel';
+$locale['Value'] = 'Hodnota';
+$locale['Values'] = 'Hodnoty';
+$locale['Vertical'] = 'Vertikální';
+$locale['Visibility'] = 'Viditelnost';
+$locale['Visible'] = 'Viditelný';
+$locale['Weather'] = 'Počasí';
+$locale['Week'] = 'Týden';
+$locale['Widget'] = 'Widget';
+$locale['Widget footer (6)'] = 'Widget zápatí (6)';
+$locale['Widget template'] = 'Widget šablony';
+$locale['Widget source'] = 'Widget zdroje';
+$locale['Widgets'] = 'Widgety';
+$locale['Widgets created here will show the average air quality calculated from all sensors.'] = 'Vytvořené widgety budou zobrazovat průměrnou kvalitu vzduchu vypočítanou ze všech senzorů.';
+$locale['Widgets created here will show the state of a single device.'] = 'Vytvořené widgety zobrazují stav jednoho zařízení';
+$locale['Year'] = 'Rok';*/
+
+$jsLocale = array();
+$locale = array();
+
+$locale['_widgets'] = [
+    0 => [
+        'label' => 'Okay',
+        'icon' => 'fa-smile-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'chůze venku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'venkovní tělesné aktivity'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'pobyt ve venkovním prostředí'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'pobyt ve venkovním prostředí (těhotné, astmatici, senioři)'
+            ],
+        ]
+    ],
+    1 => [
+        'label' => 'Moderate',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'chůze venku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'venkovní tělesné aktivity'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'trávit čas venku'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Nedoporučeno',
+                'description' => 'pobyt ve venkovním prostředí (těhotné ženy, astmatici, senioři)'
+            ],
+        ]
+    ],
+    2 => [
+        'label' => 'Mediocre',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Přijatelné',
+                'description' => 'chůze venku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Přijatelný',
+                'description' => 'venkovní fyzické aktivity'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'orange',
+                'label' => 'Nedoporučeno',
+                'description' => 'dlouhodobý pobyt na čerstvém vzduchu'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Nedoporučeno',
+                'description' => 'dlouhodobý pobyt na čerstvém vzduchu (těhotné ženy, astmatici, senioři)'
+            ],
+        ]
+    ],
+    3 => [
+        'label' => 'Unfavorable',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'zůstat doma (senioři, těhotné ženy, lidé s onemocněním srdce a dýchacích cest)'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'orange',
+                'label' => 'Nedoporučeno',
+                'description' => 'chůze venku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'orange',
+                'label' => 'Nedoporučeno',
+                'description' => 'venkovní fyzické aktivity'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'orange',
+                'label' => 'Omezení',
+                'description' => 'řízení auta'
+            ],
+        ]
+    ],
+    4 => [
+        'label' => 'Bad',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Doporučeno',
+                'description' => 'zůstat doma'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'red',
+                'label' => 'Odložit',
+                'description' => 'chůze venku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'red',
+                'label' => 'Odložit',
+                'description' => 'venkovní fyzické aktivity'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'red',
+                'label' => 'Odložit',
+                'description' => 'řízení auta'
+            ],
+        ]
+    ]
+];
+
+?>
