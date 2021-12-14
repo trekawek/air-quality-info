fd5528cb21fd8779f677b62d35a2d51fc265ce76
C. Jacobs <515451+ProBackup-nl@users.noreply.github.com>
2021-12-14 12:11:45 +0100

    Align pollution_levels limits with EU standards
    limit values aligned with EU standards https://ec.europa.eu/environment/air/quality/standards.htm

--- src/htdocs/lib/pollution_level.php
+++ src/htdocs/lib/pollution_level.php
@@ -3,6 +3,7 @@ namespace AirQualityInfo\Lib;
 
 // https://www.airqualitynow.eu/pl/about_indices_definition.php
 // TODO: consider updating ranges to align with http://powietrze.gios.gov.pl/pjp/current
+// limit values aligned with EU standards https://ec.europa.eu/environment/air/quality/standards.htm
 class PollutionLevel {
 
     const POLLUTION_LEVELS = array(
@@ -19,7 +20,7 @@ class PollutionLevel {
 
     const PM10_THRESHOLD_1Y = 40;
     const PM10_DAILY_THRESHOLD_1Y = 50;
-    const PM10_DAYS_ABOVE_THRESHOLD = 31;
+    const PM10_DAYS_ABOVE_THRESHOLD = 35;
 
     const PM25_THRESHOLDS_1H = array(0, 15, 30, 55, 110);
     const PM25_THRESHOLDS_24H = array(0, 10, 20, 30, 60);
@@ -27,10 +28,10 @@ class PollutionLevel {
     const PM25_DAILY_THRESHOLD_1Y = 30;
 
     const PM10_LIMIT_1H = 50;
-    const PM25_LIMIT_1H = 25;
+    const PM25_LIMIT_1H = 20;
 
     const PM10_LIMIT_24H = 50;
-    const PM25_LIMIT_24H = 25;
+    const PM25_LIMIT_24H = 20;
 
     public static function findLevel($thresholds, $value) {
         if ($value === null) {
