5986e084c0c9608d7c40fc0300c8be929174039d
Tomek Rękawek <trekawek@gmail.com>
2022-12-22 12:05:20 +0100

    Defer the JS map

--- src/htdocs/views/map/fullscreen.php
+++ src/htdocs/views/map/fullscreen.php
@@ -2,5 +2,5 @@
     <div id="map" class="h-100" data-hide-controls="true" data-url="<?php echo l('map', 'data') ?>"></div>
 </div>
 
-<?php echo jsLink("public/js/map.js"); ?>
+<?php echo jsLink("public/js/map.js", "defer"); ?>
 <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo CONFIG['google_maps_key'] ?>&callback=mapsLoaded" async defer></script>
