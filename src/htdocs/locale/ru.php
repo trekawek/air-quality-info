cbc17d041323ca154ef86af016f882cd999ebf9f
Tomek Rękawek <trekawek@gmail.com>
2024-01-23 17:24:42 +0100

    Support for Russian language.
    Support was kindly provided by Alexandru Luchiian.

---
+++ src/htdocs/locale/ru.php
@@ -0,0 +1,409 @@
+<?php
+$jsLocale = array();
+$locale['days'] = 'дни';
+$locale['Are you sure to delete this resource?'] = 'Вы уверены, что хотите удалить этот ресурс?';
+$locale['Days'] = 'Дни';
+$locale['Detector humidity'] = 'Датчик влажности';
+$locale['Detector temperature'] = 'Датчик температуры';
+$locale['High'] = 'Высоко';
+$locale['Humidity'] = 'Влажность';
+$locale['Low'] = 'Низко';
+$locale['Medium'] = 'Средне';
+$locale['Night view'] = 'Ночной режим';
+$locale['PM₁₀ limit'] = 'Предел PM₁₀';
+$locale['PM₂.₅ limit'] = 'Предел PM₂.₅';
+$locale['Pollution level'] = 'Уровень загрязнения';
+$locale['Pressure'] = 'Давление';
+$locale['Temperature'] = 'Температура';
+$locale['Very high'] = 'Очень высоко';
+$locale['Very low'] = 'Очень низко';
+$locale['DATE_TIME_FORMAT'] = 'DATE_TIME_FORMAT';
+$locale['DATE_FORMAT'] = 'DATE_FORMAT';
+
+$locale = array_merge($jsLocale);
+$locale['<meta> tags to be put in the <head> section'] = '<meta> теги, которые следует поместить в раздел <head>';
+$locale['%s should be between %d and %d'] = '%s должно быть между %d и %d';
+$locale['<a href="https://www.airqualitynow.eu/about_indices_definition.php">CAQI</a> index'] = '<a href="https://www.airqualitynow.eu/about_indices_definition.php">Индекс CAQI</a>';
+$locale['About page body'] = 'Текст страницы "О проекте"';
+$locale['About page name (3)'] = 'Наименование страницы "О проекте" (3)';
+$locale['Account'] = 'Аккаунт';
+$locale['Account info'] = 'Информация об аккаунте';
+$locale['Add custom field mapping'] = 'Добавить сопоставление пользовательского поля';
+$locale['Add mapping'] = 'Добавить сопоставление';
+$locale['Add new device'] = 'Добавить новое устройство';
+$locale['Add new directory'] = 'Добавить новый каталог';
+$locale['Add new widget'] = 'Добавить новый виджет';
+$locale['Add sensor id'] = 'Добавить идентификатор сенсора';
+$locale['Add your own device'] = 'Добавить свое устройство';
+$locale['Added new sensor id'] = 'Добавлен новый идентификатор сенсора';
+$locale['Air quality'] = 'Качество воздуха';
+$locale['Allows sensor.community sensors'] = 'Позволяет использовать сенсоры sensor.community';
+$locale['An account with this e-mail address is already registered.'] = 'Аккаунт с таким адресом электронной почты уже зарегистрирован.';
+$locale['Annual average'] = 'Годовой средний';
+$locale['Annual averages'] = 'Годовые показатели';
+$locale['Annual stats'] = 'Годовая статистика';
+$locale['Assign'] = 'Назначить';
+$locale['Assign device'] = 'Назначить устройство';
+$locale['Assigned the device'] = 'Назначено устройство';
+$locale['Atmospheric pressure'] = 'Атмосферное давление';
+$locale['Average'] = 'Средний';
+$locale['Back to the device'] = 'Вернуться к устройству';
+$locale['Brand name (2)'] = 'Имя бренда (2)';
+$locale['Choose location'] = 'Выбрать местоположение';
+$locale['Change your password'] = 'Изменить пароль';
+$locale['Click to see more details.'] = 'Щелкните, чтобы увидеть больше деталей.';
+$locale['Contact info'] = 'Контактная информация';
+$locale['Contact'] = 'Контакт';
+$locale['Create'] = 'Создать';
+$locale['Create directory'] = 'Создать каталог';
+$locale['Create your account'] = 'Создайте свой аккаунт';
+$locale['Create widget'] = 'Создать виджет';
+$locale['Created a new device'] = 'Создано новое устройство';
+$locale['Created a new directory'] = 'Создан новый каталог';
+$locale['Created a new mapping'] = 'Создано новое сопоставление';
+$locale['Created a new widget'] = 'Создан новый виджет';
+$locale['CSV'] = 'CSV';
+$locale['CSV archive'] = 'Архив CSV';
+$locale['CSV fields'] = 'Поля CSV';
+$locale['Custom CSS style'] = 'Пользовательский стиль CSS';
+$locale['Current air quality'] = 'Текущее качество воздуха';
+$locale['Current brand icon'] = 'Текущая иконка бренда';
+$locale['Custom field mappings'] = 'Сопоставления пользовательских полей';
+$locale['Daily graph'] = 'График за день';
+$locale['Dashboard'] = 'Панель управления';
+$locale['Darkly theme'] = 'Тема "Темно"';
+$locale['Data'] = 'Данные';
+$locale['Database field'] = 'Поле базы данных';
+$locale['Day'] = 'День';
+$locale['Days with PM₁₀ above 50µg/m³'] = 'Дни с PM₁₀ выше 50 µg/m³';
+$locale['Debug info'] = 'Информация отладки';
+$locale['Deleted the device'] = 'Удалено устройство';
+$locale['Deleted the mapping'] = 'Удалено сопоставление';
+$locale['Deleted the node'] = 'Удален узел';
+$locale['Deleted the sensor id'] = 'Удален идентификатор сенсора';
+$locale['Description'] = 'Описание';
+$locale['Default theme'] = 'Тема "Стандартно"';
+$locale['Deleted widget'] = 'Удален виджет';
+$locale['Device hierarchy'] = 'Иерархия устройств';
+$locale['Device JSON list'] = 'Список JSON устройств';
+$locale['Device list'] = 'Список устройств';
+$locale['Device name'] = 'Имя устройства';
+$locale['Device'] = 'Устройство';
+$locale['Device widget list'] = 'Список виджетов устройства';
+$locale['Devices'] = 'Устройства';
+$locale['Domain'] = 'Домен';
+$locale['Domain name'] = 'Имя домена';
+$locale['Domain widget list'] = 'Список виджетов домена';
+$locale['E-mail'] = 'Электронная почта';
+$locale['Edit sensor'] = 'Изменить сенсор';
+$locale['Edit widget'] = 'Изменить виджет';
+$locale['Enabling the sensor.community support disables the templating options.'] = 'Включение поддержки sensor.community отключает опции шаблонизации.';
+$locale['Enter e-mail to reset your password'] = 'Введите электронную почту для сброса пароля';
+$locale['ESP 8266 id'] = 'Идентификатор ESP 8266';
+$locale['ESP8266 ID'] = 'ID ESP8266';
+$locale['Edit device'] = 'Изменить устройство';
+$locale['Edit directory'] = 'Изменить каталог';
+$locale['E-mail has been sent. Please check your mailbox.'] = 'Электронная почта была отправлена. Пожалуйста, проверьте свой почтовый ящик.';
+$locale['Elevation (m a.s.l.)'] = 'Высота (м над уровнем моря)';
+$locale['Expose location in the data.json'] = 'Раскрывать местоположение в data.json';
+$locale['Extra description'] = 'Дополнительное описание';
+$locale['Fill to add a new item to the menu.'] = 'Заполните, чтобы добавить новый элемент в меню.';
+$locale['Find sensor on'] = 'Найти сенсор на';
+$locale['Forgot password'] = 'Забыли пароль';
+$locale['Forgot your password?'] = 'Забыли пароль?';
+$locale['Footer'] = 'Нижний колонтитул';
+$locale['Footer (5)'] = 'Нижний колонтитул (5)';
+$locale['Graphs'] = 'Графики';
+$locale['Guide'] = 'Руководство';
+$locale['Header (4)'] = 'Верхний колонтитул (4)';
+$locale['Hidden'] = 'Скрытый';
+$locale['Home'] = 'Главная';
+$locale['Horizontal'] = 'Горизонтальный';
+$locale['Import data from Madavi.de'] = 'Импортировать данные с Madavi.de';
+$locale['Imports data from'] = 'Импортирует данные от';
+$locale['Import finished'] = 'Импорт завершен';
+$locale['Include in Kanarek'] = 'Включить в Kanarek';
+$locale['Index'] = 'Индекс';
+$locale['Instantaneous'] = 'Мгновенный';
+$locale['Invalid domain'] = 'Неверный домен';
+$locale['Invalid email or password'] = 'Неверный адрес электронной почты или пароль';
+$locale['Invalid path'] = 'Неверный путь';
+$locale['JSON field'] = 'JSON поле';
+$locale['JSONs'] = 'JSONs';
+$locale['Last'] = 'Последний';
+$locale['Last data'] = 'Последние данные';
+$locale['Last modification'] = 'Последнее изменение';
+$locale['Last received data'] = 'Последние полученные данные';
+$locale['Last update'] = 'Последнее обновление';
+$locale['Latitude'] = 'Широта';
+$locale['Link'] = 'Ссылка';
+$locale['Link device'] = 'Привязать устройство';
+$locale['Link external device'] = 'Привязать внешнее устройство';
+$locale['Link sensor.community device'] = 'Привязать устройство sensor.community';
+$locale['Linked device'] = 'Привязанное устройство';
+$locale['Loading...'] = 'Загрузка...';
+$locale['Locations'] = 'Местоположения';
+$locale['login'] = 'Вход';
+$locale['Login'] = 'Вход';
+$locale['Logout'] = 'Выход';
+$locale['Longitude'] = 'Долгота';
+$locale['Maintenance tools'] = 'Инструменты обслуживания';
+$locale['Make device default'] = 'Сделать устройство по умолчанию';
+$locale['Map'] = 'Карта';
+$locale['Maximum allowed size is 256 kB.'] = 'Максимальный допустимый размер - 256 кБ.';
+$locale['Month'] = 'Месяц';
+$locale['Moving average'] = 'Скользящее среднее';
+$locale['Name'] = 'Имя';
+$locale['New password has been set.'] = 'Новый пароль установлен.';
+$locale['Only PNGs and SVGs are allowed'] = 'Разрешены только PNG и SVG';
+$locale['Open'] = 'Открыто';
+$locale['Operations'] = 'Операции';
+$locale['Page unavailable offline'] = 'Страница недоступна в автономном режиме';
+$locale['Password'] = 'Пароль';
+$locale['Password was successfully updated.'] = 'Пароль успешно обновлен.';
+$locale['Path'] = 'Путь';
+$locale["Please choose value between 50 and 500."] = 'Пожалуйста, выберите значение между 50 и 500.';
+$locale["Please click the link below to update your password on aqi.eco:\nhttps://%s%s"] = 'Пожалуйста, нажмите на ссылку ниже, чтобы обновить пароль на aqi.eco:\nhttps://%s%s';
+$locale["Please create an account if you don't have one already."] = 'Пожалуйста, создайте аккаунт, если у вас его еще нет.';
+$locale['Please connect to the internet to get the most recent info.'] = 'Пожалуйста, подключитесь к Интернету, чтобы получить самую свежую информацию.';
+$locale['Please paste the URL address of the device you want to link. It must be a link within aqi.eco domain, eg.: https://warsawa.aqi.eco/al-jerozolimskie'] = 'Пожалуйста, вставьте URL-адрес устройства, которое вы хотите связать. Должна быть ссылка в пределах домена aqi.eco, например: https://warsawa.aqi.eco/al-jerozolimskie';
+$locale['Please provide a valid e-mail address.'] = 'Пожалуйста, предоставьте действительный адрес электронной почты.';
+$locale['Please provide two identical passwords.'] = 'Пожалуйста, предоставьте два идентичных пароля.';
+$locale['Please use following data to configure your sensor'] = 'Пожалуйста, используйте следующие данные для настройки вашего сенсора';
+$locale['PM'] = 'PM';
+$locale['Pollution levels by days'] = 'Уровни загрязнения по дням';
+$locale['Port'] = 'Порт';
+$locale['Powered by '] = 'Работает на ';
+$locale['Radius (m)'] = 'Радиус (м)';
+$locale['Range'] = 'Диапазон';
+$locale['Reading count'] = 'Количество чтений';
+$locale['Readings'] = 'Чтения';
+$locale['Redirect home page'] = 'Перенаправление на главную страницу';
+$locale['Received JSONs'] = 'Полученные JSONs';
+$locale['register'] = 'регистр';
+$locale['Register'] = 'Регистрация';
+$locale['Register now!'] = 'Зарегистрируйтесь сейчас!';
+$locale['Remove brand icon'] = 'Удалить значок бренда';
+$locale['Repeat password'] = 'Повторите пароль';
+$locale['Reset HTTP password'] = 'Сбросить HTTP-пароль';
+$locale['Reset your password'] = 'Сбросить пароль';
+$locale['Root'] = 'Корень';
+$locale['Send data to own API'] = 'Отправить данные на свой API';
+$locale['Sensor configuration'] = 'Настройка сенсора';
+$locale['Sensor ID'] = 'Идентификатор сенсора';
+$locale['Sensor id'] = 'Идентификатор сенсора';
+$locale['Sensor options'] = 'Параметры сенсора';
+$locale['Sensord id list'] = 'Список идентификаторов сенсоров';
+$locale['Server'] = 'Сервер';
+$locale['Settings'] = 'Настройки';
+$locale['Show all'] = 'Показать все';
+$locale['Show widget on the group page'] = 'Показать виджет на странице группы';
+$locale['Show widget on the sensor page'] = 'Показать виджет на странице сенсора';
+$locale['Sign in'] = 'Войти';
+$locale['Sign in to your account'] = 'Войдите в свой аккаунт';
+$locale['Sign up'] = 'Зарегистрироваться';
+$locale['Size'] = 'Размер';
+$locale['Start'] = 'Начать';
+$locale['Stats'] = 'Статистика';
+$locale['Support'] = 'Поддержка';
+$locale['Temperature offset'] = 'Смещение температуры';
+$locale['Template widgets'] = 'Шаблон виджетов';
+$locale['Templates'] = 'Шаблоны';
+$locale['The domain name has to consists of letters and digits.'] = 'Имя домена должно состоять из букв и цифр.';
+$locale['The minimum length of the password is 8 characters.'] = 'Минимальная длина пароля - 8 символов.';
+$locale['The name should consist of alphanumeric characters and dashes'] = 'Имя должно состоять из букв, цифр и тире';
+$locale['The path should consist of alphanumeric characters, dashes and slashes'] = 'Путь должен состоять из букв, цифр, тире и слэшей';
+$locale['Theme'] = 'Тема';
+$locale['There are no data'] = 'Нет данных';
+$locale['This domain is already used.'] = 'Этот домен уже используется.';
+$locale['This is default device'] = 'Это устройство по умолчанию';
+$locale['This is the HTML content of the custom page linked above.'] = 'Это HTML-содержимое связанной выше пользовательской страницы.';
+$locale['This token is no longer valid.'] = 'Этот токен больше не действителен.';
+$locale['Time'] = 'Время';
+$locale['Timezone'] = 'Часовой пояс';
+$locale['Title'] = 'Заголовок';
+$locale['Update'] = 'Обновить';
+$locale['Update brand icon (1)'] = 'Обновить значок бренда (1)';
+$locale['Update CSV fields'] = 'Обновить поля CSV';
+$locale['Updated CSV fields'] = 'Обновлены поля CSV';
+$locale['Updated directory'] = 'Обновленный каталог';
+$locale['Update password'] = 'Обновить пароль';
+$locale['Update password on aqi.eco'] = 'Обновить пароль на aqi.eco';
+$locale['Updated settings'] = 'Обновлены настройки';
+$locale['Updated template'] = 'Обновленный шаблон';
+$locale['Updated the default device'] = 'Обновлено устройство по умолчанию';
+$locale['Updated the device'] = 'Обновлено устройство';
+$locale['Updated the password'] = 'Обновлен пароль';
+$locale['Updated widget'] = 'Обновлен виджет';
+$locale['Use PNG picture'] = 'Использовать изображение PNG';
+$locale['Use as a favicon'] = 'Использовать как фавикон';
+$locale['User'] = 'Пользователь';
+$locale['Value'] = 'Значение';
+$locale['Values'] = 'Значения';
+$locale['Vertical'] = 'Вертикально';
+$locale['Visibility'] = 'Видимость';
+$locale['Visible'] = 'Видимый';
+$locale['Weather'] = 'Погода';
+$locale['Week'] = 'Неделя';
+$locale['Widget'] = 'Виджет';
+$locale['Widget footer (6)'] = 'Футер виджета (6)';
+$locale['Widget template'] = 'Шаблон виджета';
+$locale['Widget source'] = 'Источник виджета';
+$locale['Widgets'] = 'Виджеты';
+$locale['Widgets created here will show the average air quality calculated from all sensors.'] = 'Созданные здесь виджеты будут показывать средний уровень качества воздуха, рассчитанный из всех сенсоров.';
+$locale['Widgets created here will show the state of a single device.'] = 'Созданные здесь виджеты будут показывать состояние одного устройства.';
+$locale['Year'] = 'Год';
+$locale['_widgets'] = [
+    0 => [
+        'label' => 'Хорошо',
+        'icon' => 'fa-smile-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'прогулки на свежем воздухе'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'физическая активность на улице'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'проведение времени на улице'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'проведение времени на улице (беременные, астматики, пожилые)'
+            ],
+        ]
+    ],
+    1 => [
+        'label' => 'Умеренно',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'прогулки на свежем воздухе'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'физическая активность на улице'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'проведение времени на улице'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Не рекомендуется',
+                'description' => 'проведение времени на улице (беременные, астматики, пожилые)'
+            ],
+        ]
+    ],
+    2 => [
+        'label' => 'Средне',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Приемлемо',
+                'description' => 'прогулки на свежем воздухе'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Приемлемо',
+                'description' => 'физическая активность на улице'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'orange',
+                'label' => 'Не рекомендуется',
+                'description' => 'продолжительное пребывание на свежем воздухе'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Не рекомендуется',
+                'description' => 'продолжительное пребывание на свежем воздухе (беременные, астматики, пожилые)'
+            ],
+        ]
+    ],
+    3 => [
+        'label' => 'Неблагоприятно',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'Нахождение дома (пожилые, беременные, люди с заболеваниями сердца и дыхательных путей)'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'orange',
+                'label' => 'Не рекомендуется',
+                'description' => 'прогулки на свежем воздухе'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'orange',
+                'label' => 'Не рекомендуется',
+                'description' => 'физическая активность на улице'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'orange',
+                'label' => 'Ограничить',
+                'description' => 'вождение автомобиля'
+            ],
+        ]
+    ],
+    4 => [
+        'label' => 'Плохо',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Рекомендуется',
+                'description' => 'остаться дома'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'red',
+                'label' => 'Отложить',
+                'description' => 'прогулки на свежем воздухе'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'red',
+                'label' => 'Отложить',
+                'description' => 'физическая активность на улице'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'red',
+                'label' => 'Отложить',
+                'description' => 'вождение автомобиля'
+            ],
+        ]
+    ]
+];
+
+?>
