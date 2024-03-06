8e538aec3892159030467bdcaf4bf14c7a9416ac
Tomek Rękawek <trekawek@gmail.com>
2024-03-06 21:10:59 +0100

    Support test-api domain.

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -6,7 +6,7 @@ require('boot.php');
 $host = explode(':', $_SERVER['HTTP_HOST'])[0];
 if (in_array($host, CONFIG['admin_domains'])) {
     require('routing/admin_routing.php');
-} else if ($host === ('api' . CONFIG['user_domain_suffixes'][0])) {
+} else if ($host === ('api' . CONFIG['user_domain_suffixes'][0]) || $host === ('test-api' . CONFIG['user_domain_suffixes'][0])) {
     require('routing/api_routing.php');
 } else {
     require('routing/user_routing.php');
