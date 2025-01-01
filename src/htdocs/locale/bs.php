baa214c2a2b5d0bdba172899b88ac4a3684f9c15
Namik Delilovic <namikdelilovic@gmail.com>
2025-01-02 00:41:44 +0100

    Add locale for Bosnian language

---
+++ src/htdocs/locale/bs.php
@@ -0,0 +1,416 @@
+<?php
+$jsLocale = array();
+$jsLocale['days'] = 'dani';
+$jsLocale['Ambient light'] = 'Pozadinsko osvjetljenje';
+$jsLocale['Are you sure to delete this resource?'] = 'Jeste li sigurni da želite obrisati ovaj resurs?';
+$jsLocale['Days'] = 'Dani';
+$jsLocale['Detector humidity'] = 'Detektor vlažnosti';
+$jsLocale['Detector temperature'] = 'Detektor temperature';
+$jsLocale['High'] = 'Visoko';
+$jsLocale['Humidity'] = 'Vlažnost';
+$jsLocale['Low'] = 'Nisko';
+$jsLocale['Medium'] = 'Srednje';
+$jsLocale['Night view'] = 'Noćni pregled';
+$jsLocale['PM₁₀ limit'] = 'PM₁₀ limit';
+$jsLocale['PM₂.₅ limit'] = 'PM₂.₅ limit';
+$jsLocale['Pollution level'] = 'Nivo zagađenja';
+$jsLocale['Pressure'] = 'Pritisak';
+$jsLocale['Rainfall'] = 'Padavine';
+$jsLocale['Temperature'] = 'Temperatura';
+$jsLocale['Very high'] = 'Vrlo visoko';
+$jsLocale['Very low'] = 'Vrlo nisko';
+$jsLocale['Wind speed'] = 'Brzina vjetra';
+$jsLocale['DATE_TIME_FORMAT'] = 'dd, MMM D HH:mm';
+$jsLocale['DATE_FORMAT'] = 'dd, MMM D';
+
+$locale = array_merge($jsLocale);
+$locale['<meta> tags to be put in the <head> section'] = '<meta> tagovi koje treba staviti u <head> sekciju';
+$locale['%s should be between %d and %d'] = '%s treba biti između %d i %d';
+$locale['<a href="https://www.airqualitynow.eu/about_indices_definition.php">CAQI</a> index'] = '<a href="https://www.airqualitynow.eu/about_indices_definition.php">Indeks CAQI</a>';
+$locale['About page body'] = 'Tijelo stranice "O projektu"';
+$locale['About page name (3)'] = 'Naziv stranice "O projektu" (3)';
+$locale['Account'] = 'Račun';
+$locale['Account info'] = 'Informacije o računu';
+$locale['Add custom field mapping'] = 'Dodajte prilagođeno mapiranje polja';
+$locale['Add mapping'] = 'Dodajte mapiranje';
+$locale['Add new device'] = 'Dodajte novi uređaj';
+$locale['Add new directory'] = 'Dodajte novi direktorij';
+$locale['Add new widget'] = 'Dodajte novi widget';
+$locale['Add sensor id'] = 'Dodajte ID senzora';
+$locale['Add your own device'] = 'Dodajte svoj uređaj';
+$locale['Added new sensor id'] = 'Dodan novi ID senzora';
+$locale['Air quality'] = 'Kvalitet zraka';
+$locale['Allows sensor.community sensors'] = 'Omogućuje sensor.community senzore';
+$locale['An account with this e-mail address is already registered.'] = 'Račun s ovom e-mail adresom je već registriran.';
+$locale['Annual average'] = 'Godišnji prosjek';
+$locale['Annual averages'] = 'Godišnji prosjeci';
+$locale['Annual stats'] = 'Godišnja statistika';
+$locale['Assign'] = 'Dodijeli';
+$locale['Assign device'] = 'Dodijelite uređaj';
+$locale['Assigned the device'] = 'Uređaj je dodijeljen';
+$locale['Atmospheric pressure'] = 'Atmosferski pritisak';
+$locale['Average'] = 'Prosjek';
+$locale['Back to the device'] = 'Nazad na uređaj';
+$locale['Brand name (2)'] = 'Naziv marke (2)';
+$locale['Choose location'] = 'Odaberite lokaciju';
+$locale['Change your password'] = 'Promijenite lozinku';
+$locale['Click to see more details.'] = 'Kliknite za više detalja.';
+$locale['Contact info'] = 'Kontakt informacije';
+$locale['Contact'] = 'Kontakt';
+$locale['Create'] = 'Kreiraj';
+$locale['Create directory'] = 'Kreirajte direktorij';
+$locale['Create your account'] = 'Kreirajte svoj račun';
+$locale['Create widget'] = 'Kreirajte widget';
+$locale['Created a new device'] = 'Kreiran novi uređaj';
+$locale['Created a new directory'] = 'Kreiran novi direktorij';
+$locale['Created a new mapping'] = 'Kreirano novo mapiranje';
+$locale['Created a new widget'] = 'Kreiran novi widget';
+$locale['CSV'] = 'CSV';
+$locale['CSV archive'] = 'CSV arhiva';
+$locale['CSV fields'] = 'CSV polja';
+$locale['Custom CSS style'] = 'Prilagođeni CSS stil';
+$locale['Custom CSS style for widget'] = 'Prilagođeni CSS stil za widget';
+$locale['Current air quality'] = 'Trenutni kvalitet zraka';
+$locale['Current brand icon'] = 'Trenutna ikona brenda';
+$locale['Custom field mappings'] = 'Prilagođeni mapiranja polja';
+$locale['Daily graph'] = 'Dnevni grafikon';
+$locale['Dashboard'] = 'Kontrolna tabla';
+$locale['Darkly theme'] = 'Tema Tamno';
+$locale['Data'] = 'Podaci';
+$locale['Database field'] = 'Polje baze podataka';
+$locale['Day'] = 'Dan';
+$locale['Days with PM₁₀ above 50µg/m³'] = 'Dani s PM₁₀ iznad 50 µg/m³';
+$locale['Debug info'] = 'Informacije za otklanjanje grešaka';
+$locale['Deleted the device'] = 'Uređaj je obrisan';
+$locale['Deleted the mapping'] = 'Mapiranje je obrisano';
+$locale['Deleted the node'] = 'Čvor je obrisan';
+$locale['Deleted the sensor id'] = 'ID senzora je obrisan';
+$locale['Description'] = 'Opis';
+$locale['Default theme'] = 'Zadana tema';
+$locale['Deleted widget'] = 'Widget je obrisan';
+$locale['Device hierarchy'] = 'Hijerarhija uređaja';
+$locale['Device JSON list'] = 'JSON lista uređaja';
+$locale['Device list'] = 'Lista uređaja';
+$locale['Device name'] = 'Naziv uređaja';
+$locale['Device'] = 'Uređaj';
+$locale['Device widget list'] = 'Lista widgeta uređaja';
+$locale['Devices'] = 'Uređaji';
+$locale['Domain'] = 'Domena';
+$locale['Domain name'] = 'Naziv domene';
+$locale['Domain widget list'] = 'Lista widgeta domene';
+$locale['E-mail'] = 'E-mail';
+$locale['Edit sensor'] = 'Uredite senzor';
+$locale['Edit widget'] = 'Uredite widget';
+$locale['Enabling the sensor.community support disables the templating options.'] = 'Omogućavanje podrške za sensor.community onemogućava opcije za template.';
+$locale['Enter e-mail to reset your password'] = 'Unesite e-mail za resetiranje lozinke';
+$locale['ESP 8266 id'] = 'ESP 8266 id';
+$locale['ESP8266 ID'] = 'ESP8266 ID';
+$locale['Edit device'] = 'Uredite uređaj';
+$locale['Edit directory'] = 'Uredite direktorij';
+$locale['E-mail has been sent. Please check your mailbox.'] = 'E-mail je poslan. Provjerite svoju poštu.';
+$locale['Elevation (m a.s.l.)'] = 'Nadmorska visina (m n.v.)';
+$locale['Expose location in the data.json'] = 'Prikaži lokaciju u data.json';
+$locale['Extra description'] = 'Dodatni opis';
+$locale['Fill to add a new item to the menu.'] = 'Ispunite kako biste dodali novu stavku u izbornik.';
+$locale['Find sensor on'] = 'Pronađi senzor na';
+$locale['Forgot password'] = 'Zaboravili ste lozinku';
+$locale['Forgot your password?'] = 'Zaboravili ste lozinku?';
+$locale['Footer'] = 'Podnožje';
+$locale['Footer (5)'] = 'Podnožje (5)';
+$locale['Graphs'] = 'Grafikoni';
+$locale['Guide'] = 'Vodič';
+$locale['Header (4)'] = 'Zaglavlje (4)';
+$locale['Hidden'] = 'Skriven';
+$locale['Home'] = 'Početna';
+$locale['Horizontal'] = 'Horizontalno';
+$locale['If you device has stopped sending data, please check <a href="https://aqi.eco/news">the news page</a>.'] = 'Ako vaš uređaj prestane slati podatke, molimo provjerite <a href="https://aqi.eco/news">stranicu s novostima</a>';
+$locale['Import data from Madavi.de'] = 'Improtuj podatke sa Madavi.de';
+$locale['Imports data from'] = 'Importira podatke iz';
+$locale['Import finished'] = 'Import završen';
+$locale['Include in Kanarek'] = 'Uključi u Kanarek';
+$locale['Index'] = 'Indeks';
+$locale['Instantaneous'] = 'Trenutačno';
+$locale['Invalid domain'] = 'Neispravna domena';
+$locale['Invalid email or password'] = 'Neispravan e-mail ili lozinka';
+$locale['Invalid path'] = 'Neispravan path';
+$locale['It is only required for the devices which are not configured to push data to sensor.community.'] = 'To je potrebno samo za uređaje koji nisu konfigurirani za slanje podataka na sensor.community.';
+$locale['JSON field'] = 'JSON polje';
+$locale['JSONs'] = 'JSONi';
+$locale['Last'] = 'Posljednji';
+$locale['Last data'] = 'Posljednji podaci';
+$locale['Last modification'] = 'Posljednja izmjena';
+$locale['Last received data'] = 'Posljednji primljeni podaci';
+$locale['Last update'] = 'Posljednje ažuriranje';
+$locale['Latitude'] = 'Širina';
+$locale['Link'] = 'Veza';
+$locale['Link device'] = 'Poveži uređaj';
+$locale['Link external device'] = 'Poveži vanjski uređaj';
+$locale['Link sensor.community device'] = 'Poveži sensor.community uređaj';
+$locale['Linked device'] = 'Povezani uređaj';
+$locale['Loading...'] = 'Učitavanje...';
+$locale['Locations'] = 'Lokacije';
+$locale['login'] = 'prijava';
+$locale['Login'] = 'Prijava';
+$locale['Logout'] = 'Odjava';
+$locale['Longitude'] = 'Dužina';
+$locale['Maintenance tools'] = 'Alati za održavanje';
+$locale['Make device default'] = 'Postavi uređaj kao zadani';
+$locale['Map'] = 'Mapa';
+$locale['Maximum allowed size is 256 kB.'] = 'Maksimalna dozvoljena veličina je 256 kB.';
+$locale['Month'] = 'Mjesec';
+$locale['Moving average'] = 'Pokretni prosjek';
+$locale['Name'] = 'Ime';
+$locale['New password has been set.'] = 'Nova lozinka je postavljena.';
+$locale['News'] = 'Vijesti';
+$locale['Only PNGs and SVGs are allowed'] = 'Dozvoljeni su samo PNG i SVG';
+$locale['Open'] = 'Otvoreno';
+$locale['Operations'] = 'Operacije';
+$locale['Page unavailable offline'] = 'Stranica nedostupna van mreže';
+$locale['Password'] = 'Lozinka';
+$locale['Password was successfully updated.'] = 'Lozinka je uspješno ažurirana.';
+$locale['Path'] = 'Path';
+$locale["Please choose value between 50 and 500."] = 'Molimo odaberite vrijednost između 50 i 500.';
+$locale["Please click the link below to update your password on aqi.eco:\nhttps://%s%s"] = 'Molimo kliknite na donji link za ažuriranje vaše lozinke na aqi.eco:\nhttps://%s%s';
+$locale["Please create an account if you don't have one already."] = 'Molimo kreirajte račun ako ga već nemate.';
+$locale['Please connect to the internet to get the most recent info.'] = 'Molimo spojite se na internet kako biste dobili najnovije informacije.';
+$locale['Please paste the URL address of the device you want to link. It must be a link within aqi.eco domain, eg.: https://warsawa.aqi.eco/al-jerozolimskie'] = 'Molimo zalijepite URL adresu uređaja koji želite povezati. Mora biti veza unutar domene aqi.eco, npr.: https://warsawa.aqi.eco/al-jerozolimskie';
+$locale['Please provide a valid e-mail address.'] = 'Molimo dostavite važeću e-mail adresu.';
+$locale['Please provide two identical passwords.'] = 'Molimo dostavite dvije iste lozinke.';
+$locale['Please use following data to configure your sensor'] = 'Molimo koristite sljedeće podatke za konfiguraciju vašeg senzora';
+$locale['PM'] = 'PM';
+$locale['Pollution levels by days'] = 'Nivoi zagađenja po danima';
+$locale['Port'] = 'Port';
+$locale['Powered by '] = 'Pokreće ';
+$locale['Radius (m)'] = 'Radijus (m)';
+$locale['Range'] = 'Raspon';
+$locale['Reading count'] = 'Broj očitavanja';
+$locale['Readings'] = 'Očitavanja';
+$locale['Redirect home page'] = 'Preusmjeri na početnu stranicu';
+$locale['Received JSONs'] = 'Primljeni JSONi';
+$locale['register'] = 'registar';
+$locale['Register'] = 'Registracija';
+$locale['Register now!'] = 'Registrujte se sada!';
+$locale['Remove brand icon'] = 'Ukloni brend ikonu';
+$locale['Repeat password'] = 'Ponovite lozinku';
+$locale['Reset HTTP password'] = 'Resetuj HTTP lozinku';
+$locale['Reset your password'] = 'Resetuj lozinku';
+$locale['Root'] = 'Korijen';
+$locale['Send data to own API'] = 'Pošalji podatke na vlastiti API';
+$locale['Sensor configuration'] = 'Konfiguracija senzora';
+$locale['Sensor ID'] = 'ID senzora';
+$locale['Sensor id'] = 'ID senzora';
+$locale['Sensor options'] = 'Opcije senzora';
+$locale['Sensord id list'] = 'Lista ID-ja senzora';
+$locale['Server'] = 'Server';
+$locale['Settings'] = 'Postavke';
+$locale['Show all'] = 'Pokaži sve';
+$locale['Show widget on the group page'] = 'Pokaži widget na stranici grupe';
+$locale['Show widget on the sensor page'] = 'Pokaži widget na stranici senzora';
+$locale['Sign in'] = 'Prijava';
+$locale['Sign in to your account'] = 'Prijavite se na svoj račun';
+$locale['Sign up'] = 'Registriraj se';
+$locale['Size'] = 'Veličina';
+$locale['Start'] = 'Početak';
+$locale['Stats'] = 'Statistika';
+$locale['Support'] = 'Podrška';
+$locale['Temperature offset'] = 'Pomak temperature';
+$locale['Template widgets'] = 'Template widgeti';
+$locale['Templates'] = 'Templatei';
+$locale['The domain name has to consists of letters and digits.'] = 'Naziv domene mora sadržavati slova i brojeve.';
+$locale['The minimum length of the password is 8 characters.'] = 'Minimalna dužina lozinke je 8 znakova.';
+$locale['The name should consist of alphanumeric characters and dashes'] = 'Ime treba sadržavati alfanumeričke znakove i crtice';
+$locale['The path should consist of alphanumeric characters, dashes and slashes'] = 'Put treba sadržavati alfanumeričke znakove, crtice i kosu crtu';
+$locale['Theme'] = 'Tema';
+$locale['There are no data'] = 'Nema podataka';
+$locale['This domain is already used.'] = 'Ova domena je već u upotrebi.';
+$locale['This is default device'] = 'Ovo je zadani uređaj';
+$locale['This is the HTML content of the custom page linked above.'] = 'Ovo je HTML sadržaj prilagođene stranice povezan gore.';
+$locale['This token is no longer valid.'] = 'Ovaj token više nije važeći.';
+$locale['Time'] = 'Vrijeme';
+$locale['Timezone'] = 'Vremenska zona';
+$locale['Title'] = 'Naslov';
+$locale['Update'] = 'Ažuriraj';
+$locale['Update brand icon (1)'] = 'Ažuriraj brend ikonu (1)';
+$locale['Update CSV fields'] = 'Ažuriraj CSV polja';
+$locale['Updated CSV fields'] = 'Ažurirana CSV polja';
+$locale['Updated directory'] = 'Ažuriran direktorij';
+$locale['Update password'] = 'Ažuriraj lozinku';
+$locale['Update password on aqi.eco'] = 'Ažuriraj lozinku na aqi.eco';
+$locale['Updated settings'] = 'Ažurirane postavke';
+$locale['Updated template'] = 'Ažuriran template';
+$locale['Updated the default device'] = 'Ažuriran zadani uređaj';
+$locale['Updated the device'] = 'Ažuriran uređaj';
+$locale['Updated the password'] = 'Ažurirana lozinka';
+$locale['Updated widget'] = 'Ažuriran widget';
+$locale['Use PNG picture'] = 'Koristi PNG sliku';
+$locale['Use as a favicon'] = 'Koristi kao favikonu';
+$locale['User'] = 'Korisnik';
+$locale['Value'] = 'Vrijednost';
+$locale['Values'] = 'Vrijednosti';
+$locale['Vertical'] = 'Vertikalno';
+$locale['Visibility'] = 'Vidljivost';
+$locale['Visible'] = 'Vidljivo';
+$locale['Weather'] = 'Vrijeme';
+$locale['Week'] = 'Sedmica';
+$locale['Widget'] = 'Widget';
+$locale['Widget footer (6)'] = 'Podnožje widgeta (6)';
+$locale['Widget template'] = 'Widget template';
+$locale['Widget source'] = 'Izvor widgeta';
+$locale['Widgets'] = 'Widgeti';
+$locale['Widgets created here will show the average air quality calculated from all sensors.'] = 'Widgeti kreirani ovdje će pokazati prosječan kvalitet zraka izračunat od svih senzora.';
+$locale['Widgets created here will show the state of a single device.'] = 'Widgeti kreirani ovdje će pokazati stanje pojedinačnog uređaja.';
+$locale['Year'] = 'Godina';
+$locale['_widgets'] = [
+    0 => [
+        'label' => 'Dobro',
+        'icon' => 'fa-smile-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'šetnje na svježem zraku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'fizička aktivnost napolju'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'provođenje vremena napolju'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'provođenje vremena napolju (trudnice, astmatičari, stariji)'
+            ],
+        ]
+    ],
+    1 => [
+        'label' => 'Umjereno',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'šetnje na svježem zraku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'fizička aktivnost napolju'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'provođenje vremena napolju'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Ne preporučuje se',
+                'description' => 'provođenje vremena napolju (trudnice, astmatičari, stariji)'
+            ],
+        ]
+    ],
+    2 => [
+        'label' => 'Srednje',
+        'icon' => 'fa-meh-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'green',
+                'label' => 'Prihvatljivo',
+                'description' => 'šetnje na svježem zraku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'green',
+                'label' => 'Prihvatljivo',
+                'description' => 'fizička aktivnost napolju'
+            ],
+            [
+                'icon' => 'icon-tree',
+                'color' => 'orange',
+                'label' => 'Ne preporučuje se',
+                'description' => 'dugotrajno provođenje na svježem zraku'
+            ],
+            [
+                'icon' => 'icon-elderly',
+                'color' => 'orange',
+                'label' => 'Ne preporučuje se',
+                'description' => 'dugotrajno provođenje na svježem zraku (trudnice, astmatičari, stariji)'
+            ],
+        ]
+    ],
+    3 => [
+        'label' => 'Nepovoljno',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'Boravak kod kuće (stariji, trudnice, osobe sa srčanim i respiratornim problemima)'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'orange',
+                'label' => 'Ne preporučuje se',
+                'description' => 'šetnje na svježem zraku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'orange',
+                'label' => 'Ne preporučuje se',
+                'description' => 'fizička aktivnost napolju'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'orange',
+                'label' => 'Ograničiti',
+                'description' => 'vožnju automobila'
+            ],
+        ]
+    ],
+    4 => [
+        'label' => 'Loše',
+        'icon' => 'fa-frown-o',
+        'recommendations' => [
+            [
+                'icon' => 'icon-home',
+                'color' => 'green',
+                'label' => 'Preporučuje se',
+                'description' => 'ostati kod kuće'
+            ],
+            [
+                'icon' => 'icon-spacer',
+                'color' => 'red',
+                'label' => 'Odložiti',
+                'description' => 'šetnje na svježem zraku'
+            ],
+            [
+                'icon' => 'icon-rower',
+                'color' => 'red',
+                'label' => 'Odložiti',
+                'description' => 'fizička aktivnost napolju'
+            ],
+            [
+                'icon' => 'icon-car',
+                'color' => 'red',
+                'label' => 'Odložiti',
+                'description' => 'vožnju automobila'
+            ],
+        ]
+    ]
+];
+
+?>
